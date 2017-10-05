b7fcce441a6ac8c721de55a339077f23fc0a834a
dctalbot <dctalbot@umich.edu>
2017-10-05 00:42:36 -0400

    initial setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""Bootleg python package configuration."""
+
+from setuptools import setup
+
+setup(
+    name='bootleg',
+    version='0.1.0',
+    install_requires=[
+        'flask',
+        'click'
+    ],
+)
